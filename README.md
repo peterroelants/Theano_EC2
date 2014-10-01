91eb4d0532ec998aaf04b83f6b4f98434a18415c
peter <peter.roelants@gmail.com>
2014-10-01 11:19:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Amazon EC2 Theano instance #
+
+Script to run on an Amazon EC2 instance with a GPU to set up Theano
+
+
+## Startup an Amazon EC2 instance ##
+
+Search for the [*Amazon Linux AMI with NVIDIA GRID GPU Driver*](https://aws.amazon.com/marketplace/pp/B00FYCDDTE) AMI by Nvidia on the AWS marketplace. Launch this AMI on a GPU instance (e.g. *g2.2xlarge*).
+
+## Install Theano ##
+
+SSH into the shell of the newly created instance, and install git via:
+
+    sudo yum install git
+
+Get the setup script by running:
+
+    git clone https://github.com/peterroelants/Theano_EC2.git
+
+And run this script via:
+
+    sudo sh setup.sh
+
---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# update yum
+sudo yum -y update
+
+# install gcc compilers
+sudo yum -y install
+	gcc \
+	gcc-gfortran \
+	gcc-c++
+# install numerical libraries
+sudo yum -y install \
+	blas-devel \
+	lapack-devel \
+	atlas-devel
+# install python libraries
+# http://www.scipy.org/install.html
+sudo yum -y install \
+	python-devel \
+	python-nose \
+	python-setuptools \
+	numpy \
+	scipy \
+	python-matplotlib \
+	ipython \
+	python-nose \
+	python-pip
+
+# install theano
+# http://deeplearning.net/software/theano/install_centos6.html
+sudo pip install Theano
+
+# Startup the Nvidia CUDA application in root so that it becomes available
+# http://docs.nvidia.com/cuda/cuda-getting-started-guide-for-linux/index.html#runfile-verifications
+# http://stackoverflow.com/a/24665354/919431
+sudo sh cuda_startup.sh
+
+# test the theano installation
+# http://deeplearning.net/software/theano/tutorial/using_gpu.html
+echo
+echo '------------------'
+echo 'Test Theano on CPU'
+echo '------------------'
+THEANO_FLAGS=mode=FAST_RUN,device=cpu,floatX=float32 python check1.py
+
+echo
+echo '------------------'
+echo 'Test Theano on GPU'
+echo '------------------'
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python check1.py
+
+echo
